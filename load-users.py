92c93f2faf123c8cf277a54afcaaeed6af4adeb1
aankit <aankit.patel@gmail.com>
2014-11-19 17:09:59 -0500

    fixed load-users oauth

--- load-users.py
+++ load-users.py
@@ -7,6 +7,6 @@ from sqlalchemy.exc import OperationalError
 from sqlalchemy.orm.exc import MultipleResultsFound, NoResultFound
 
 #set up twitter api
-twitter_auth = twitter.oauth.OAuth(keys.lu.OATH_TOKEN, keys.lu.OAUTH_TOKEN_SECRET, keys.lu.CONSUMER_KEY, keys.lu.CONSUMER_SECRET)
+twitter_auth = twitter.oauth.OAuth(keys.lu.OAUTH_TOKEN, keys.lu.OAUTH_TOKEN_SECRET, keys.lu.CONSUMER_KEY, keys.lu.CONSUMER_SECRET)
 api = twitter.Twitter(auth=twitter_auth)
 
