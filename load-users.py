d7ddab48c4da7fba37a382f1d6707b778f65984a
aankit <aankit.patel@gmail.com>
2014-11-19 17:08:11 -0500

    building user query and save'

---
+++ load-users.py
@@ -0,0 +1,12 @@
+#saving some select user data from twitter
+
+import twitter, keys.lu
+from tweetsql.model import Friend, User
+from tweetsql.database import Base, db_session, engine
+from sqlalchemy.exc import OperationalError
+from sqlalchemy.orm.exc import MultipleResultsFound, NoResultFound
+
+#set up twitter api
+twitter_auth = twitter.oauth.OAuth(keys.lu.OATH_TOKEN, keys.lu.OAUTH_TOKEN_SECRET, keys.lu.CONSUMER_KEY, keys.lu.CONSUMER_SECRET)
+api = twitter.Twitter(auth=twitter_auth)
+
