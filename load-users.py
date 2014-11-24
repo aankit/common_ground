ae009b71392c4af7c39bd6506e79af8a862c3610
aankit <aankit.patel@gmail.com>
2014-11-24 11:05:02 -0500

    using all keys for get users

--- load-users.py
+++ load-users.py
@@ -1,13 +1,32 @@
 #saving some select user data from twitter
 
-import twitter, keys.lu, sys
+import twitter, keys.lu, keys.lf1, keys.lf2, keys.ls, sys
 from tweetsql.model import UserData, User
 from tweetsql.database import Base, db_session, engine
 from sqlalchemy.exc import OperationalError
 from sqlalchemy.orm.exc import MultipleResultsFound, NoResultFound
 
+keys = [keys.lu, keys.lf1, keys.lf2, keys.ls]
+
+def get_rate_limit(t, data='remaining'):
+    limit = t.application.rate_limit_status()
+    return limit['resources']['users']['/users/lookup'][data]
+
+def get_clean_key():
+	maxRemaining = 0
+	winners = ''
+	for k in keys:
+		twitter_auth = twitter.oauth.OAuth(k.OAUTH_TOKEN, k.OAUTH_TOKEN_SECRET, k.CONSUMER_KEY, k.CONSUMER_SECRET)
+		api = twitter.Twitter(auth=twitter_auth)
+		remaining = get_rate_limit(t=api)
+		if remaining > maxRemaining:
+			maxRemaining = remaining
+			winner = k
+	return winner
+
 #set up twitter api
-twitter_auth = twitter.oauth.OAuth(keys.lu.OAUTH_TOKEN, keys.lu.OAUTH_TOKEN_SECRET, keys.lu.CONSUMER_KEY, keys.lu.CONSUMER_SECRET)
+clean_key = get_clean_key()
+twitter_auth = twitter.oauth.OAuth(clean_key.OAUTH_TOKEN, clean_key.OAUTH_TOKEN_SECRET, clean_key.CONSUMER_KEY, clean_key.CONSUMER_SECRET)
 api = twitter.Twitter(auth=twitter_auth)
 
 got_data = db_session.query(UserData.user_id).all() #first get all users in the friend table
@@ -17,17 +36,13 @@ no_data = [(pk,uid) for pk,uid in all_users if pk not in got_data]
 #group no_data by 100 for our twitter call
 no_data_100s = [no_data[i:i+99] for i in range(0,len(no_data),100)] 
 
-def get_rate_limit(t, data='remaining'):
-    limit = t.application.rate_limit_status()
-    return limit['resources']['users']['/users/lookup'][data]
-
-if get_rate_limit(t=api, data='remaining')==0:
+if get_rate_limit(api)==0:
 	print 'rate limit hit'
 	print 'try again at %d' %get_rate_limit(t=api, data='reset')
-	sys.exit()
+
 
 for l in no_data_100s:
-	if get_rate_limit(t=api, data='remaining')==0:
+	if get_rate_limit(api)==0:
 		print 'rate limit hit'
 		print 'try again at %d' %get_rate_limit(t=api, data='reset')
 		break
